5aac5e0d9d30709a422c618da452ed4ad4f7fb2d
Manuel486 <82180428+Manuel486@users.noreply.github.com>
2022-11-04 19:58:20 -0500

    Primer commit

---
+++ service/client-service.js
@@ -0,0 +1,49 @@
+//Abrir http(método,url)
+
+// CRUD    -  Métodos HTTP
+// Create  - POST
+// Read    - GET
+// Update  - PUT/PATCH
+// Delete  - DELETE
+
+// Fetch API
+// si no le ponemos nada por defecto es GET
+const listaClientes = () => 
+    fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
+
+// const listaClientes = () => {
+//     // promisse es una funcion asincrona, no vamos a esperar 
+//     // hasta que termine para ejecutar el resto del código
+//     const promise = new Promise((resolve,reject)=>{
+//         const http = new XMLHttpRequest();
+//         http.open("GET","http://localhost:3000/perfil");
+//         http.send();
+//         http.onload = () => {
+//             const response = JSON.parse(http.response);
+//             // Si es mayor a 400 significa que hubo un error
+//             if(http.status >= 400){
+//                 reject(response);
+//             } else {
+//                 resolve(response);
+//             }
+//         };
+//     });
+//     return promise;
+// }
+
+const crearCliente = (nombre,email) => {
+    return fetch("http://localhost:3000/perfil",{
+        method : "POST",
+        headers: {
+            "Content-type" : "application/json"
+        },
+        body : JSON.stringify({nombre,email,id: uuid.v4()})
+    });
+};
+
+export const clientServices = {
+    listaClientes,
+    crearCliente,
+};
+
+
